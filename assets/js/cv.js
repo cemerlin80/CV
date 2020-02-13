8ab1b11c061459331a741cdc70e723176be0a909
80010-58-18 <80010-58-18@exchange.ad.afpanet>
2020-02-13 15:58:44 +0100

    first commit

---
+++ assets/js/cv.js
@@ -0,0 +1,52 @@
+
+
+// Animation parallax (effet image 3d en background)
+$(document).ready(function(){
+  $('.parallax').parallax();
+
+});
+
+
+$(document).ready(function(){
+  $('.sidenav').sidenav();
+});
+// Cacher la navbar côté gauche
+
+  // Animation pour le slider -> accueil et compétences
+
+  $('.slider').slider({
+    fullwidth: true,
+    indicators: true,
+    height: 600
+    });
+
+// Modal du right caption (profil)
+
+
+$(document).ready(function(){
+  $('.modal').modal();
+});
+
+// indicators -> true = montre les boutons , false = retire les boutons
+// Réglage de la hauteur du slide et de l'animation
+
+
+
+// Animation du scroll
+
+$(document).ready(function(){
+  $('.scrollspy').scrollSpy();
+});
+
+// Effet accordéon dans éxpériences
+$(document).ready(function(){
+  $('.collapsible').collapsible();
+});
+     
+         
+
+
+
+
+
+      
---
+++ cv.js
@@ -0,0 +1,36 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//     var elems = document.querySelectorAll('.autocomplete');
+//     var instances = M.Autocomplete.init(elems, options);
+//   });
+
+
+//   // Or with jQuery
+
+//   $(document).ready(function(){
+//     $('input.autocomplete').autocomplete({
+//       data: {
+//         "Apple": null,
+//         "Microsoft": null,
+//         "Google": 'https://placehold.it/250x250'
+//       },
+//     });
+//   });
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.parallax');
+    var instances = M.Parallax.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.parallax').parallax();
+  });
+
+  $('a','nav').mouseover(function(){
+    $('span','nav').animate({
+        'width': $(this).width(),
+         'left': $(this).position().left}
+    ,'fast')
+})
